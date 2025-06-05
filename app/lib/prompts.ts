64deb7677a29e2f0218adfb13bc9a5aad50c209a
ykchen03 <ykchen03@gmail.com>
2025-06-05 16:40:47 +0800

    Delete app directory

--- app/lib/prompts.ts
+++
@@ -1,8 +0,0 @@
-export const systemPrompts = {
-    chat: "You are a helpful chatbot.",
-    code: "You are a programming assistant. Respond with complete code examples.",
-    parkFinder: "",
-    debug: "",
-}
-
-export type PromptTypes = "debug" | "chat" | "code" | "parkFinder";
